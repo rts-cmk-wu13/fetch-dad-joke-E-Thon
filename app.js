e726f87805784c019e63e748fe3d945a6f90bc6a
Emilie Thon <emilie.m.thon@gmail.com>
2025-02-06 12:18:54 +0100

    files added

---
+++ app.js
@@ -0,0 +1,10 @@
+fetch("https://icanhazdadjoke.com", {
+    headers: {
+        "Accept": "application/json"
+    }
+})
+    .then(response => response.json())
+    .then(data => 
+        console.log(data)
+
+    )
