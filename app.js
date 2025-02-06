e0802c41ac9bfcff8e5c50dfb948c7120306d3cd
Emilie Thon <emilie.m.thon@gmail.com>
2025-02-06 12:26:53 +0100

    random joke fetched and added to html in the div created in JS

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-fetch("https://icanhazdadjoke.com", {
-    headers: {
-        "Accept": "application/json"
-    }
+fetch("https://icanhazdadjoke.com/", {
+  headers: {
+    Accept: "application/json",
+  },
 })
-    .then(response => response.json())
-    .then(data => 
-        console.log(data)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    let body = document.querySelector("body");
+    let jokeElm = document.createElement("div");
+    jokeElm.classList.add("jokes");
 
-    )
+    jokeElm.innerHTML = `
+    ${data.joke}
+    `
+
+
+    body.appendChild(jokeElm);
+  });
